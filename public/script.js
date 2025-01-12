07e0ac897c0ccfa3ecc314164fb8fa29387ff975
FR-Ananas <thcplnd@gmail.com>
2025-01-12 18:44:47 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -17,13 +17,29 @@ document.getElementById('popup').style.display = 'flex';
 
 // Gérer la saisie du pseudo
 document.getElementById('set-username-button').addEventListener('click', () => {
-    username = document.getElementById('username').value.trim();
-    if (username) {
+    const inputUsername = document.getElementById('username').value.trim();
+    if (inputUsername) {
+        username = inputUsername; // Récupérer le pseudo
         document.getElementById('popup').style.display = 'none'; // Masquer le popup
         document.getElementById('chat-window').style.display = 'block'; // Afficher la fenêtre de chat
         socket.emit('new user', username); // Envoyer le pseudo au serveur
     } else {
         alert("Veuillez entrer un pseudo valide.");
+    }
+});
+
+// Écoute de la touche "Entrée" pour soumettre le pseudo
+document.getElementById('username').addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        const inputUsername = document.getElementById('username').value.trim();
+        if (inputUsername) {
+            username = inputUsername; // Récupérer le pseudo
+            document.getElementById('popup').style.display = 'none'; // Masquer le popup
+            document.getElementById('chat-window').style.display = 'block'; // Afficher la fenêtre de chat
+            socket.emit('new user', username); // Envoyer le pseudo au serveur
+        } else {
+            alert("Veuillez entrer un pseudo valide.");
+        }
     }
 });
 
