d9e02c8ffd0a4985b72197df39bfbb6bf27ee870
FR-Ananas <thcplnd@gmail.com>
2025-01-19 16:25:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,46 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
+const socketIo = require('socket.io');
+
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+const io = socketIo(server);
 
-app.use(express.static('public'));
+let users = [];
 
-const users = new Set();
+// Serve le fichier HTML
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/chat.html');
+});
 
+// Gestion des connexions des utilisateurs
 io.on('connection', (socket) => {
+  console.log('Un utilisateur est connecté');
+  
+  // Écoute des nouveaux utilisateurs
   socket.on('newUser', (username) => {
-    users.add(username);
-    io.emit('userList', Array.from(users));
+    users.push(username);
+    io.emit('userList', users);
   });
 
+  // Écoute des messages
   socket.on('message', (data) => {
     io.emit('message', data);
   });
 
+  // Écoute de la déconnexion
+  socket.on('disconnectUser', (username) => {
+    users = users.filter(user => user !== username);
+    io.emit('userList', users);
+  });
+
+  // Lorsque l'utilisateur se déconnecte
   socket.on('disconnect', () => {
-    users.delete(socket.username);
-    io.emit('userList', Array.from(users));
+    console.log('Un utilisateur est déconnecté');
   });
 });
 
+// Démarre le serveur sur le port 3000
 server.listen(3000, () => {
-  console.log('Serveur lancé sur le port 3000');
+  console.log('Serveur démarré sur le port 3000');
 });
